27ded2079db39eda491bbbc84fb5f6021942db2e
Hafthor93 <hafthor93@gmail.com>
2022-03-19 11:44:11 +0000

    card namer update
    updated card names

--- lib/functions/card_namer.dart
+++ lib/functions/card_namer.dart
@@ -6,6 +6,36 @@ it should return the name of the corresponding card (Ignore the suit)
  */
 
 String CardNamer(int i){
+  if(i == 1){
+    return "A";
+  } else if (i == 2){
+    return "Tvistur";
+  } else if (i == 3){
+    return "Þristur";
+  } else if (i == 4){
+    return "Fjarki";
+  } else if (i == 5){
+    return "Fimma";
+  } else if (i == 6){
+    return "Sexa";
+  } else if (i == 7){
+    return "Sjöa";
+  } else if (i == 8){
+    return "Átta";
+  } else if (i == 9){
+    return "Nía";
+  } else if (i == 10){
+    return "Tía";
+  } else if (i == 11){
+    return "J";
+  } else if (i == 12){
+    return "Q";
+  } else if (i == 13){
+    return "K";
+  }
+
+
+
 
 
 }
